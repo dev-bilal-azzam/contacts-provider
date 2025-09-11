664adf2606b73994909bc8afbd71d38775ea635e
bilalazzam <blalazam065@gmail.com>
2025-09-12 01:54:18 +0300

    exposed api for instantiating ContactsProvider

--- contacts-provider/src/commonMain/kotlin/com/bilalazzam/contacts_provider/rememberContactsProvider.kt
+++ contacts-provider/src/commonMain/kotlin/com/bilalazzam/contacts_provider/rememberContactsProvider.kt
@@ -2,5 +2,6 @@ package com.bilalazzam.contacts_provider
 
 import androidx.compose.runtime.Composable
 
+// exposed api for instantiating ContactsProvider
 @Composable
 expect fun rememberContactsProvider(): ContactsProvider
